0d73e169e8b3d88ffbc3e1fe02c1e374d30146ea
jonnywintech <jonnywintech@gmail.com>
2023-10-21 18:17:45 +0200

    fixed increment

--- resources/js/editImages.js
+++ resources/js/editImages.js
@@ -13,11 +13,11 @@ window.addEventListener("load", function () {
 
     let deleteButtons = document.querySelectorAll(".delete-image");
 
-    let totalImages =
-        document.querySelectorAll(".img-position").length + 2 ?? 0;
+    let totalImages = document.querySelectorAll(".img-position").length;
 
     addButton.addEventListener("click", () => {
-        container.insertAdjacentHTML("afterbegin", image);
+        totalImages++; // Increment the totalImages count
+        container.insertAdjacentHTML("afterbegin", createImage(totalImages));
         // update delete buttons
         reloadButtons();
     });
@@ -35,7 +35,7 @@ window.addEventListener("load", function () {
     };
 
     function reloadButtons() {
-        // it clean excess  event listeners when new image is created
+        // it cleans excess event listeners when a new image is created
         deleteButtons = document.querySelectorAll(".delete-image");
 
         deleteButtons.forEach((button) => {
@@ -57,47 +57,48 @@ window.addEventListener("load", function () {
         galleryUrlPaste.value = galleryUrlCopy.value;
     });
 
-    let image = `
-                            <div class="col">
-                                <div class="card shadow-sm">
-                                    <img
-                                    src="https://via.placeholder.com/640x480.png/003399?text=cats+Faker+enim"
-                                    alt="img" />
-                                    <div class="card-body">
-                                    <div class="input-group mb-1">
-                                        <span class="input-group-text" id="inputGroup-sizing-default"
-                                        >URL:</span
-                                        >
-                                        <input
-                                        type="text"
-                                        id="image-url"
-                                        name="image_new[]"
-                                        class="form-control"
-                                        value="" />
-                                    </div>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="input-group mb-1">
-                                        <span class="input-group-text" id="inputGroup-sizing-default"
-                                            >Image position:</span
-                                        >
-
-                                        <input
-                                            type="number"
-                                            class="img-position form-control"
-                                            id="position"
-                                            name="position_new[]"
-                                            value="${totalImages}" />
-                                        </div>
-                                        <div class="btn-group mb-1">
-                                        <button
-                                            type="button"
-                                            class="btn btn-sm btn-outline-danger delete-image ms-1 outline-danger">
-                                            Delete
-                                        </button>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                  `;
+    function createImage(imagePosition) {
+        return `
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img
+                    src="https://via.placeholder.com/640x480.png/003399?text=cats+Faker+enim"
+                    alt="img" />
+                    <div class="card-body">
+                    <div class="input-group mb-1">
+                        <span class="input-group-text" id="inputGroup-sizing-default"
+                        >URL:</span
+                        >
+                        <input
+                        type="text"
+                        id="image-url"
+                        name="image_new[]"
+                        class="form-control"
+                        value="" />
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="input-group mb-1">
+                        <span class="input-group-text" id="inputGroup-sizing-default"
+                            >Image position:</span
+                        >
+                        <input
+                            type="number"
+                            class="img-position form-control"
+                            id="position"
+                            name="position_new[]"
+                            value="${imagePosition}" />
+                        </div>
+                        <div class="btn-group mb-1">
+                        <button
+                            type="button"
+                            class="btn btn-sm btn-outline-danger delete-image ms-1 outline-danger">
+                            Delete
+                        </button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
 });
